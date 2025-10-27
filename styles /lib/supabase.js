873d155963e8e7e29b5b280c4222704b0863ac85
Noahable-Offcial <Noahable.yt@gmail.com>
2025-10-27 10:07:02 +0000

    Create supabase.js

---
+++ styles /lib/supabase.js
@@ -0,0 +1,8 @@
+import { createClient } from '@supabase/supabase-js';
+
+const supabaseUrl = 'https://tohpkqtsqfjypkbzlyez.supabase.co';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRvaHBrcXRzcWZqeXBrYnpseWV6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjE1NTE4OTAsImV4cCI6MjA3NzEyNzg5MH0.SrkcW1mZP4S3R87Xwu4yDCqbSryxVS2dbbmvf0FvnbY
+';
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+
